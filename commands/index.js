707750d896da46f876cd6d86f182436f96ae9a47
vivaxy <xyxuye2007@126.com>
2016-11-27 13:54:51 +0800

    feat(commit): check if a git tree is clean

--- commands/index.js
+++ commands/index.js
@@ -4,6 +4,7 @@
  */
 
 import isAGitRepository from '../lib/isAGitRepository';
+import isGitStatusClean from '../lib/isGitStatusClean';
 import * as console from '../lib/console';
 import acp from '../lib/acp';
 import prompt from '../lib/prompt';
@@ -11,6 +12,10 @@ import prompt from '../lib/prompt';
 export default async() => {
     if (!isAGitRepository()) {
         console.error(`not a git repository`);
+        process.exit(1);
+    }
+    if (isGitStatusClean()) {
+        console.error(`nothing to commit, working tree clean`);
         process.exit(1);
     }
     const commitMessage = await prompt();

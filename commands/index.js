3793d68e2a31881ee50e18c3ad0d62e01d707863
vivaxy <xyxuye2007@126.com>
2016-11-27 15:21:06 +0800

    feat(git): :sparkles:skip tasks or add task according to git status

--- commands/index.js
+++ commands/index.js
@@ -8,26 +8,26 @@ import Listr from 'listr';
 import isGitRepository from '../git/status/isGitRepository';
 import gitClean from '../git/status/gitClean';
 import needGitPush from '../git/status/needGitPush';
+import gitRemote from '../git/status/gitRemote';
 import * as console from '../lib/console';
 import gitAdd from '../git/commands/gitAdd';
 import gitCommit from '../git/commands/gitCommit';
 import gitPush from '../git/commands/gitPush';
 import prompt from '../lib/prompt';
 
-const needGitAddOrCommit = !gitClean;
-
 const prepare = () => {
+
+    const needGitAddOrCommit = !gitClean;
+
     if (!isGitRepository) {
         console.error(`not a git repository`);
         process.exit(1);
     }
     if (!needGitAddOrCommit) {
         if (!needGitPush) {
-            console.error(`nothing to commit, working tree clean`);
+            console.error(`nothing to commit or push, working tree clean`);
             process.exit(1);
         }
-        gitPush();
-        process.exit(0);
     }
 };
 
@@ -37,22 +37,37 @@ export default async() => {
 
     const commitMessage = await prompt();
 
-    const listr = new Listr([
+    const tasks = [
         {
             title: `git add`,
             task: gitAdd,
+            skip: () => {
+                if (gitClean) {
+                    return `nothing to commit, working tree clean`;
+                }
+            },
         },
         {
             title: `git commit`,
             task: () => {
                 return gitCommit(commitMessage);
             },
+            skip: () => {
+                if (gitClean) {
+                    return `nothing to commit, working tree clean`;
+                }
+            },
         },
-        {
+    ];
+
+    const listr = new Listr(tasks);
+
+    if (gitRemote) {
+        listr.add({
             title: `git push`,
             task: gitPush,
-        }
-    ]);
+        });
+    }
 
     try {
         await listr.run();

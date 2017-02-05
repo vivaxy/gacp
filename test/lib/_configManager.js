a21ffbee21ce27666a14a7a73dc5e24c15b2f5e3
vivaxy <xyxuye2007@126.com>
2017-02-05 12:55:03 +0800

    fix(configManager): :bug:Fix gitmoji stat order

---
+++ test/lib/_configManager.js
@@ -0,0 +1,8 @@
+/**
+ * @since 2017-02-05 12:52
+ * @author vivaxy
+ */
+
+import { readListByStatOrder } from '../../lib/configManager';
+
+console.log(readListByStatOrder());
---
+++ test/lib/configManager.js
@@ -0,0 +1,8 @@
+/**
+ * @since 2017-02-05 12:49
+ * @author vivaxy
+ */
+
+require('babel-polyfill');
+require('babel-register')();
+require('./_configManager');

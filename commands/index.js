92373d954f1e45c6f2541d364263a3b10733a4a1
maguowei <maguowei@meituan.com>
2019-06-13 17:10:37 +0800

    fix(push): :bug:function

--- commands/index.js
+++ commands/index.js
@@ -41,7 +41,7 @@ const prepare = async () => {
 };
 
 const runGitPush = async (push) => {
-  return push ? await gitPush : true;
+  return push ? await gitPush() : true;
 };
 
 const runTasks = async ({ push }) => {
